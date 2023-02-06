dc24f12b91c6008e1a8d2f7fe5eda0740b808592
MaciejDubowik <maciejxpoland@gmail.com>
2023-02-06 23:36:09 +0100

    chapter8 done

--- EggTimer/EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ EggTimer/EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -7,9 +7,35 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
-
+    let eggTimes: [String : Int] = ["Soft": 10, "Medium": 420, "Hard": 720]
+    var totalTime: Float = 0
+    var secondsPassed: Float = 0
+    
+    @IBOutlet weak var progresBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle!
+        let result = eggTimes[hardness]!
+        var counter = result
+        while(counter >= 0){
+            counter-=1
+            secondsPassed+=1
+            progresBar.progress = 1.0 - (secondsPassed/Float(result))
+            Thread.sleep(forTimeInterval: 1.0)
+            if(counter == 0){
+                titleLabel.text = "Done"
+                
+            }
+        }
+    
+        
+    }
+    
 
 }
