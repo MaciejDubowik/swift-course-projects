c3f96c8a9c22947511f4030aaf6fc1b1f3cf264b
MaciejDubowik <maciejxpoland@gmail.com>
2023-04-19 22:19:54 +0200

    hello world

---
+++ Diamond App/Diamond App/Diamond_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diamond_AppApp.swift
+//  Diamond App
+//
+//  Created by Maciej Dubowik on 19/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Diamond_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
