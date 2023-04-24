2e22e5a1e2d401548cc291b3aaa99dcc9d01342f
MaciejDubowik <maciejxpoland@gmail.com>
2023-04-24 22:38:35 +0200

    initial commit

---
+++ Dice/Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Maciej Dubowik on 24/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
